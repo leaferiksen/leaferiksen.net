138d104fe48bd5151f3035b9fb837d47a7c3ed0c
Leaf Eriksen <leaf@Leafs-MBP.local>
2024-09-22 22:32:18 -0700

    refactor

--- glow.js
+++ glow.js
@@ -1,12 +1,12 @@
 window.onload = function () {
-    setTimeout(glowStart, 1000);
+  setTimeout(glowStart, 1000);
 };
 function glowStart() {
-    var toggleLabel = document.getElementById('toggle-label');
-    toggleLabel.classList += ('glow');
-    setTimeout(glowEnd, 2000);
+  var toggleLabel = document.getElementById("toggle-label");
+  toggleLabel.classList += "glow";
+  setTimeout(glowEnd, 2000);
 }
 function glowEnd() {
-    var toggleLabel = document.getElementById('toggle-label');
-    toggleLabel.classList -= ('glow');
+  var toggleLabel = document.getElementById("toggle-label");
+  toggleLabel.classList -= "glow";
 }
