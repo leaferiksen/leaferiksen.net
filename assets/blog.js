233188a61bd557282fa73415e4ae3353399a2704
Leaf Eriksen <leaf@Leafs-MBP.local>
2024-09-27 16:00:21 -0700

    blog js fixes

--- assets/blog.js
+++ assets/blog.js
@@ -1,20 +1,24 @@
 //open the accordion for the current url hash
 window.onload = function openAccordion() {
   const hash = window.location.hash;
-  const accord = document.querySelector(`details${hash}`);
-  if (accord) {
-    accord.open = true;
+  if (hash) {
+    const accord = document.querySelector(`details${hash}`);
+    if (accord) {
+      accord.open = true;
+    }
+  } else {
+    return; // Exit the function when there's an empty hash
   }
 };
 
-function addurl(hash) {
+function addurl(newhash) {
   // Close all currently open <details> elements except the one we want to open.
   const details = document.querySelectorAll("details");
   details.forEach((detail) => {
-    if (detail.id !== hash && detail.open === true) {
+    if (detail.id !== newhash && detail.open === true) {
       detail.open = false;
     }
   });
   // add the hash to the url
-  location.href = "blog.html#" + hash;
+  location.href = "blog.html#" + newhash;
 }
