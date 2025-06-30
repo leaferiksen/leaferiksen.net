75b81c7a7804f8720c10916e44e14fe85d8a1ef6
Leaf Eriksen <leaferiksen@gmail.com>
2025-06-29 21:05:54 -0700

    strict mode and arrow functions

---
+++ assets/blog.js
@@ -0,0 +1,23 @@
+"use strict";
+//open the accordion for the current url hash
+window.onload = () => {
+	const hash = window.location.hash;
+	if (hash) {
+		const accordion = document.querySelector(`details${hash}`);
+		if (accordion) accordion.open = true;
+	}
+};
+
+const addurl = (newhash) => {
+	// When client opens an accordion close any other open accordions.
+	const allAccordions = document.querySelectorAll("details");
+	allAccordions.forEach((accordion) => {
+		if (accordion.id !== newhash && accordion.open === true)
+			accordion.open = false;
+	});
+	// Is client local? If so, include html file extension when adding the hash to the url
+	location.href =
+		window.location.protocol === "file:"
+			? "blog.html#" + newhash
+			: "blog#" + newhash;
+};
